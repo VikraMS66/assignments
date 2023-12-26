d00fb1cfb1eaa4c4c17d18420c80278321fe707d
VikaMS66 <vikramsingh966722@gmail.com>
2023-12-26 17:39:34 +0530

    01-js easy assignment done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,28 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
 
+  if (str1.length == 0 && str2.length == 0) {
+    return true;
+  }
+  if (str1.length != str2.length) {
+    return false;
+  }
+  let flag = false;
+  for (let j = 0; j < str1.length; j++) {
+    flag = false;
+    for (let i = 0; i < str2.length; i++) {
+      if (str1[j] == str2[i]) {
+        flag = true;
+      }
+    }
+    if (flag == false) {
+      break;
+    }
+  }
+  return flag;
 }
 
 module.exports = isAnagram;
